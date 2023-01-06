7496ec84788895f6ba51a54b2bd17b868a9356a2
jiyeong <bjiyeong1216@gmail.com>
2023-01-06 17:57:14 +0900

    no message

--- src/main/java/com/project/space/spaceinfo/mapper/SpaceInfoMapper.java
+++ src/main/java/com/project/space/spaceinfo/mapper/SpaceInfoMapper.java
@@ -30,6 +30,10 @@ public interface SpaceInfoMapper {
 	public int SpaceInfoDelete(int pnum);
 	//등록자별 공간가져오기
 	public List<Space_InfoVO> selectBySpaceInfo(String userid);
+	public List<Space_InfoVO> selectByUserid(String userid);
+	public String selectByh_code(int h_code);
+	public List<String> GetAllSpaceNameByUserid(String userid);
+	public Space_InfoVO selectByuseridSname(Map<String, String> map);
 		
 		
 }
