ec52215a12c158e921c4c1370efc21c97ca51698
9ony <darsta7@gmail.com>
2023-01-14 15:06:10 +0900

    SPACE-14 관리자페이지 회원목록에서 해당회원 클릭시 예약했던 내역을 팝업으로 표시 대시보드 틀 구성함

--- src/main/java/com/project/space/admin/mapper/AdminMapper.java
+++ src/main/java/com/project/space/admin/mapper/AdminMapper.java
@@ -3,6 +3,7 @@ package com.project.space.admin.mapper;
 import java.util.List;
 import java.util.Map;
 
+import com.project.space.admin.service.AdminMemberHistoryVO;
 import com.project.space.admin.service.AdminMemberInquiryVO;
 import com.project.space.admin.service.AdminSpaceInquiryVO;
 import com.project.space.domain.HashtagVO;
@@ -21,4 +22,12 @@ public interface AdminMapper {
 	public List<String> getSpaceAddr();
 	
 	public List<AdminSpaceInquiryVO> searchSpaceByFilter(Map<String, String> filter);
+	
+	public List<AdminMemberHistoryVO> getUserHistory(String userid);
+	
+	public List<AdminMemberInquiryVO> todayJoinMember(String string);
+	
+	public List<AdminSpaceInquiryVO> todayInsertSpace(String string);
+	
+	public List<AdminSpaceInquiryVO> todayPopSpace(String string);
 }
